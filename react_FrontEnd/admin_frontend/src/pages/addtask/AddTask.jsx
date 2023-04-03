b8ca5882ab6bdbfe4a0e51dc660b18701e2059a3
NamanSnag <nnag44141@gmail.com>
2023-04-04 01:15:14 +0530

    add new task done & project completed

--- react_FrontEnd/admin_frontend/src/pages/addtask/AddTask.jsx
+++ react_FrontEnd/admin_frontend/src/pages/addtask/AddTask.jsx
@@ -1,18 +1,40 @@
-import React from 'react'
-import {GrAdd} from 'react-icons/gr'
+import React, { useState } from "react";
+import { GrAdd } from "react-icons/gr";
+import axios from "axios";
 
-import './style.scss'
+import "./style.scss";
 
 const AddTask = () => {
-  return (
-    <div className='addtask'>
-     <div className='addtask__form'>
-        <h1>ADD TASK</h1>
-        <input type='text' className='addtask__input' placeholder='Enter task' />
-        <button className='addtask__btnn'><GrAdd className='addtask__btn'/>ADD</button>
-     </div>
-    </div>
-  )
-}
+  const [task, setTask] = useState(null);
 
-export default AddTask
+  // add new task
+  const handleAddTask = async () => {
+    try {
+      const res = await axios.post("/addTask", { task: task });
+      console.log(res.data.message);
+      alert(res.data.message);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="addtask">
+      <div className="addtask__form">
+        <h1>ADD TASK</h1>
+        <input
+          type="text"
+          className="addtask__input"
+          placeholder="Enter task"
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button className="addtask__btnn">
+          <GrAdd className="addtask__btn" onClick={handleAddTask} />
+          ADD
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default AddTask;
